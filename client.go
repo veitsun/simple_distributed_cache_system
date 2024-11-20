435ca22af09cb7e519a6497b5cc55200ed8b5ec5
sunwei <sw20002000@163.com>
2024-11-20 11:08:57 +0800

    sunwei_end

--- client.go
+++ client.go
@@ -1,34 +1,45 @@
 package main
---xianzai 黄师兄的
-import (
-		"context"
-		"fmt"
-    "time"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
-    pb "sunwei.top/cacheserver/cache"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "sunwei.top/cacheserver/cache"
 )
 
 func setupClient() {
 	var opts []grpc.DialOption
-  var err error
+	var err error
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	conn[0], err = grpc.Dial(address[2], opts...)
 	if err != nil {
 		fmt.Println("fail to dial: %v", err)
 	}
-  fmt.Println("Set up client for", address[2])
+	fmt.Println("Set up client for", address[2])
 
 	conn[1], err = grpc.Dial(address[3], opts...)
 	if err != nil {
 		fmt.Println("fail to dial: %v", err)
 	}
-  fmt.Println("Set up client for", address[3])
+	fmt.Println("Set up client for", address[3])
 
-  client[0] = pb.NewCacheClient(conn[0])
-  client[1] = pb.NewCacheClient(conn[1])
+	client[0] = pb.NewCacheClient(conn[0])
+	client[1] = pb.NewCacheClient(conn[1])
+
+}
+
+// rpc client Set request
+func CacheSet(client pb.CacheClient, req *pb.SetRequest) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err := client.SetCache(ctx, req)
+	if err != nil {
+		fmt.Println("client.SetCache failed.")
+	}
 }
 
 // rpc client Get request
@@ -36,19 +47,7 @@ func CacheGet(client pb.CacheClient, req *pb.GetRequest) (interface{}, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	getval, err := client.GetCache(ctx, req) //getval is a pointer
-	if err != nil {
-		fmt.Println("client.GetCache failed.")
-	}
-}
-
-// rpc client Set request
-func CacheSet(client pb.CacheClient, req *pb.SetRequest) (interface{}, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	_, err := client.SetCache(ctx, req)
-	if err != nil {
-		fmt.Println("client.SetCache failed.")
-	}
+	return getval.Value, err
 }
 
 // rpc client Delete request
--- server.go
+++ server.go
@@ -1,5 +1,5 @@
 package main
--- 现在黄师兄的
+
 import (
 	"context"
 	"fmt"
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"regexp"
-
 	"google.golang.org/grpc"
 	pb "sunwei.top/cacheserver/cache"
 )
@@ -20,32 +19,32 @@ var client [2]pb.CacheClient // 2 rpc client to communicate with the other 2 rpc
 var conn [2]*grpc.ClientConn // 2 connection for 2 rpc client
 
 func setAddress() {
-    if os.Args[1] == "1" { // set address variable by server index
-        address[0] = "127.0.0.1:9527" // this server's http server port 
-        address[1] = "127.0.0.1:9550" // this server's rpc server port
+	if os.Args[1] == "1" { // set address variable by server index
+		address[0] = "127.0.0.1:9527" // this server's http server port
+		address[1] = "127.0.0.1:9540" // this server's rpc server port
 
-        address[2] = "127.0.0.1:9551" // another server's rpc server port
-        address[3] = "127.0.0.1:9552" // another server's rpc server port
-    } else if os.Args[1] == "2" {
-        address[0] = "127.0.0.1:9528"
-        address[1] = "127.0.0.1:9551"
+		address[2] = "127.0.0.1:9541" // another server's rpc server port
+		address[3] = "127.0.0.1:9542" // another server's rpc server port
+	} else if os.Args[1] == "2" {
+		address[0] = "127.0.0.1:9528"
+		address[1] = "127.0.0.1:9541"
 
-        address[2] = "127.0.0.1:9550"
-        address[3] = "127.0.0.1:9552"
-    } else if os.Args[1] == "3" {
-        address[0] = "127.0.0.1:9529"
-        address[1] = "127.0.0.1:9552"
+		address[2] = "127.0.0.1:9540"
+		address[3] = "127.0.0.1:9542"
+	} else if os.Args[1] == "3" {
+		address[0] = "127.0.0.1:9529"
+		address[1] = "127.0.0.1:9542"
 
-        address[2] = "127.0.0.1:9550"
-        address[3] = "127.0.0.1:9551"
-    } else {
-        fmt.Println("only 3 cacheserver.")
-    }
+		address[2] = "127.0.0.1:9540"
+		address[3] = "127.0.0.1:9541"
+	} else {
+		fmt.Println("only 3 cacheserver.")
+	}
 }
 
 // http Get handler
 func handleGet(w http.ResponseWriter, key string) {
-    fmt.Println("get", key)
+	fmt.Println("get", key)
 
 	if _, ok := server.cache[key]; ok {
 		w.WriteHeader(http.StatusOK)
@@ -59,64 +58,36 @@ func handleGet(w http.ResponseWriter, key string) {
 		return
 	} else //if we can't find in this machine ,we search the another two
 	{
-		val1, err1 := CacheGet(client[0], &pb.GetRequest{Key: key})
-		if err1 != nil {
-			val2, err2 := CacheGet(client[1], &pb.GetRequest{Key: key})
-			if err2 != nil {
-				w.WriteHeader(http.StatusNotFound)
-			} else {
-				w.WriteHeader(http.StatusOK)
-				w.Header().Set("Content-Type", "application/json")
-				cachedValue, ok := val2.(string)
-				if !ok {
-					fmt.Println("Cached value is not a string")
-					return
-				}
-				fmt.Fprintln(w, "{\""+key+"\":\""+cachedValue+"\"}")
-				return
-			}
-		} else {
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			cachedValue, ok := val1.(string)
-			if !ok {
-				fmt.Println("Cached value is not a string")
-				return
-			}
-			fmt.Fprintln(w, "{\""+key+"\":\""+cachedValue+"\"}")
-			return
-		}
-    }
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
-
 // http Set handler
 func handleSet(w http.ResponseWriter, jsonstr string) {
+	reg := regexp.MustCompile(`{\s*"(.*)"\s*:\s*"(.*)"|\[(.*)\]|(\d+)\s*}`)
+	if reg == nil {
+		fmt.Println("regexp err")
+		return
+	}
+	result := reg.FindAllStringSubmatch(jsonstr, -1)
+	key, value := result[0][1], result[0][2]
 
-    // reg := regexp.MustCompile(`{\s*"(.*)"\s*:\s*"(.*)"\s*}`)
-    reg := regexp.MustCompile(`{\s*"(.*?)"\s*:\s*(\[(.*?)\]|"(.*?)"|(\d+))\s*}`)
-    if reg == nil { 
-        fmt.Println("regexp err")
-        return
-    }
-    result := reg.FindAllStringSubmatch(jsonstr, -1)
-    key, value := result[0][1], result[0][2]
+	fmt.Println("set", key, ":", value)
 
-    fmt.Println("set", key, ":", value)
+	server.cache[key] = value
+	CacheSet(client[0], &pb.SetRequest{Key: key, Value: value})
+    CacheSet(client[1], &pb.SetRequest{Key: key, Value: value})
 
-    server.cache[key] = value
-    CacheSet(client[0], &pb.SetRequest{Key:key, Value:value})
-    CacheSet(client[1], &pb.SetRequest{Key:key, Value:value})
+	w.WriteHeader(http.StatusOK)
 
-    w.WriteHeader(http.StatusOK)
 }
 
 // http Delete handler
 func handleDelete(w http.ResponseWriter, key string) {
-    fmt.Println("delete", key)
-    if _, ok := server.cache[key]; ok {
+	fmt.Println("delete", key)
+	if _, ok := server.cache[key]; ok {
         delete(server.cache, key)
-        CacheDelete(client[0], &pb.DeleteRequest{Key:key})
-        CacheDelete(client[1], &pb.DeleteRequest{Key:key})
+        CacheDelete(client[0], &pb.DeleteRequest{Key: key})
+        CacheDelete(client[1], &pb.DeleteRequest{Key: key})
 
         w.WriteHeader(http.StatusOK)
         fmt.Fprintln(w, "1")
@@ -125,30 +96,31 @@ func handleDelete(w http.ResponseWriter, key string) {
 
     w.WriteHeader(http.StatusOK)
     fmt.Fprintln(w, "0")
+
 }
 
 // http request handler
 func handleHttpRequest(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodGet {
-        handleGet(w, r.URL.String()[1:])
-    } else if r.Method == http.MethodPost {
-        body, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-            http.Error(w, "Unable to read request body.", http.StatusInternalServerError)
-            return
-        }
-        handleSet(w, string(body))
-    } else if r.Method == http.MethodDelete {
-        handleDelete(w, r.URL.String()[1:])
-    } else {
-        http.Error(w, "Unsupport http request.", http.StatusMethodNotAllowed)
-    }
+	if r.Method == http.MethodGet {
+		handleGet(w, r.URL.String()[1:])
+	} else if r.Method == http.MethodPost {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body.", http.StatusInternalServerError)
+			return
+		}
+		handleSet(w, string(body))
+	} else if r.Method == http.MethodDelete {
+		handleDelete(w, r.URL.String()[1:])
+	} else {
+		http.Error(w, "Unsupport http request.", http.StatusMethodNotAllowed)
+	}
 }
 
-// cacheserver type 
+// cacheserver type
 type cacheServer struct {
-    pb.UnimplementedCacheServer
-    cache map[string]interface{}
+	pb.UnimplementedCacheServer
+	cache map[string]interface{}
 }
 
 // rpc server Get handler
@@ -174,58 +146,55 @@ func (s *cacheServer) GetCache(ctx context.Context, req *pb.GetRequest) (*pb.Get
 }
 
 // rpc server Set handler
-func (s *cacheServer) SetCache(ctx context.Context, req *pb.SetRequest) (*pb.SetReply, error) {
+func (s *cacheServer) SetCache (ctx context.Context, req *pb.SetRequest) (*pb.SetReply, error) {
     s.cache[req.Key] = req.Value
     return &pb.SetReply{}, nil
 }
 
 // rpc server Delete handler
 func (s *cacheServer) DeleteCache(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteReply, error) {
-    if _, ok := s.cache[req.Key]; ok {
-        delete(s.cache, req.Key)
-        return &pb.DeleteReply{Num: 1}, nil
-    }
-    return &pb.DeleteReply{Num: 0}, nil
+	if _, ok := s.cache[req.Key]; ok {
+		delete(s.cache, req.Key)
+		return &pb.DeleteReply{Num: 1}, nil
+	}
+	return &pb.DeleteReply{Num: 0}, nil
 }
 
 func startHttpServer() {
-    http.HandleFunc("/", handleHttpRequest)
-    fmt.Println("Listening http on", address[0])
-    err := http.ListenAndServe(address[0], nil)
-    if err != nil {
-        fmt.Println("Listten failed:", err)
-    }
+	http.HandleFunc("/", handleHttpRequest)
+	fmt.Println("Listening http on", address[0])
+	err := http.ListenAndServe(address[0], nil)
+	if err != nil {
+		fmt.Println("Listten failed:", err)
+	}
 }
 
 func startRpcServer() {
-    fmt.Println("Listening rpc on", address[1])
-    lis, err := net.Listen("tcp", address[1])
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
+	fmt.Println("Listening rpc on", address[1])
+	lis, err := net.Listen("tcp", address[1])
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-    var opts []grpc.ServerOption
-    grpcServer := grpc.NewServer(opts...)
-    server = cacheServer{cache: make(map[string]interface{})}
-    pb.RegisterCacheServer(grpcServer, &server)
-    grpcServer.Serve(lis)
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	server = cacheServer{cache: make(map[string]interface{})}
+	pb.RegisterCacheServer(grpcServer, &server)
+	grpcServer.Serve(lis)
 }
 
 
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("please specify server index(1-3).")
-        return
-    }
+	if len(os.Args) != 2 {
+		fmt.Println("please specify server index(1-3).")
+		return
+	}
 
-    setAddress()
-    go startHttpServer()
-    go startRpcServer()
-    setupClient()
+	setAddress()
+	go startHttpServer()
+	go startRpcServer()
+	setupClient()
 
-    select {}
+	select {}
 }
-
-
-
